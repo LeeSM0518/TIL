7e5ab05e8989d6360406f8bc41e416c093b28037
nalsm98 <nalsm98@naver.com>
2018-09-25 16:43:24 +0900

    자료구조 세미나 준비

--- Data_Structure/python/ch3_Linked_list/LinkedList.py
+++ Data_Structure/python/ch3_Linked_list/LinkedList.py
@@ -1,6 +1,6 @@
 class LinkedListNode():
     data = int(0)
-    pNLink = {'id': 0 }
+    pLink = {'CurrentClass': 0, 'NextClass':0 }
 
 class LinkedList():
     currentCount = int(0)
@@ -8,3 +8,23 @@ class LinkedList():
 
 def creatLinkedList(self):
     self = LinkedList()
+    self.pLink['CurrentClass'] = self
+
+def getLinkedListData(pList, position):
+    i = 0
+    pCurrentNode = LinkedList()
+    for i in position:
+        pCurrentNode = pCurrentNode.pLink['NextClass']
+
+    return pCurrentNode.data
+
+def addLinkedListData(pList, position, data):
+    i = 0
+    pNewNode = LinkedListNode()
+    pPreNode = LinkedListNode()
+
+    pNewNode.data = data
+
+    pPreNode = pList.headerNode
+    for i in position - 1 :
+        pPreNode = pPreNode.pLink['NextClass']
--- python/Python_2ch_datatype/dictionary_practice.py
+++ python/Python_2ch_datatype/dictionary_practice.py
@@ -7,3 +7,14 @@ for key, value in a.items():
 
 #key 유무 확인
 print('name이 a에 들어잇는가?',('name' in a))
+
+# dictionry , id ( Data_Structure 연습 )
+pLink = { 'CurrentId': 0 , 'NextId' : 0}
+print(pLink['CurrentId'])
+pLink['CurrentId'] = id(pLink)
+print(pLink['CurrentId'])
+print(type(pLink['CurrentId']))
+pLink['CurrentId'] = pLink
+print(pLink['CurrentId'])
+print(id(pLink['CurrentId']))
+print(type(pLink['CurrentId']))
