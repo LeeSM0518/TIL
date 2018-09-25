a2eaae59250aad7e61ea39bb61ba8113e361b894
Sangmin Lee <nalsm98@naver.com>
2018-09-25 15:18:10 +0900

    자료구조 세미나 과제 및 공부

---
+++ Data_Structure/python/ch3_Linked_list/LinkedList.py
@@ -0,0 +1,10 @@
+class LinkedListNode():
+    data = int(0)
+    pNLink = {'id': 0 }
+
+class LinkedList():
+    currentCount = int(0)
+    headerNode = LinkedListNode()
+
+def creatLinkedList(self):
+    self = LinkedList()
