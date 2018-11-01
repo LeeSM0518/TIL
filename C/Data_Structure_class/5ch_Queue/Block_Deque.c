cea35e907cf3b0a585521dcab3e593951b03e251
nalsm98 <nalsm98@naver.com>
2018-11-01 11:57:43 +0900

    자료구조 수업 Queue 코드 저장

--- C/Data_Structure_class/5ch_Queue/Block_Deque.c
+++ C/Data_Structure_class/5ch_Queue/Block_Deque.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "2.h"
+#include "Deque.h"
 
 int is_empty(DequeType *dq)
 {
@@ -12,6 +12,8 @@ int is_empty(DequeType *dq)
 void init(DequeType *dq)
 {
 	dq->head = dq->tail = NULL;
+	dq->result = 0;
+	dq->broken = 0;
 }
 
 void error(char *message)
@@ -19,7 +21,7 @@ void error(char *message)
 	printf("%s\n", message);
 }
 
-DlistNode *create_node(DlistNode *llink, element item, DlistNode *rlink)
+DlistNode *create_node(DlistNode *llink, Block item, DlistNode *rlink)
 {
 	DlistNode *node = (DlistNode*)malloc(sizeof(DlistNode));
 	if (node == NULL)
@@ -30,30 +32,53 @@ DlistNode *create_node(DlistNode *llink, element item, DlistNode *rlink)
 	return node;
 }
 
-void add_rear(DequeType *dq, element item)
+void add_rear(DequeType *dq, Block item)
 {
 	DlistNode *new_node = create_node(dq->tail, item, NULL);
-	if (is_empty(dq))
+	if (is_empty(dq)) {
 		dq->head = new_node;
-	else
-		dq->tail->rlink = new_node;
-	dq->tail = new_node;
+		dq->tail = new_node;
+	}
+	else {
+		if (dq->tail->data.color == item.color)
+		{
+			dq->result += dq->tail->data.grade * item.grade;
+			delete_rear(dq);
+			dq->broken++;
+		}
+		else {
+			dq->tail->rlink = new_node;
+			dq->tail = new_node;
+		}
+	}
+	
 }
 
-void add_front(DequeType *dq, element item)
+void add_front(DequeType *dq, Block item)
 {
 	DlistNode *new_node = create_node(NULL, item, dq->head);
-	
-	if (is_empty(dq))
+
+	if (is_empty(dq)) {
 		dq->tail = new_node;
-	else
-		dq->head->llink = new_node;
-	dq->head = new_node;
+		dq->head = new_node;
+	}
+	else {
+		if(dq->head->data.color==item.color){
+			dq->result += dq->head->data.grade *item.grade;
+			delete_front(dq);
+			dq->broken++;
+		}
+		else {
+			dq->head->llink = new_node;
+			dq->head = new_node;
+		}
+	}
+	
 }
 
-element delete_front(DequeType *dq)
+Block delete_front(DequeType *dq)
 {
-	element item=NULL;
+	Block item;
 	DlistNode *removed_node;
 
 	if (is_empty(dq))
@@ -68,12 +93,12 @@ element delete_front(DequeType *dq)
 		else
 			dq->head->llink = NULL;
 	}
-	return item;
+	
 }
 
-element delete_rear(DequeType *dq)
+Block delete_rear(DequeType *dq)
 {
-	element item=NULL;
+	Block item;
 	DlistNode *removed_node;
 
 	if (is_empty(dq))
@@ -89,7 +114,7 @@ element delete_rear(DequeType *dq)
 		else
 			dq->tail->rlink = NULL;
 	}
-	return item;
+	
 }
 
 
@@ -98,7 +123,7 @@ void display(DequeType *dq)
 	DlistNode *p;
 	printf("(");
 	for (p = dq->head; p != NULL; p = p->rlink) {
-		printf("%d ", p->data);
+		printf(" %d %c", p->data.grade, p->data.color);
 	}
 	printf(")\n");
 }
