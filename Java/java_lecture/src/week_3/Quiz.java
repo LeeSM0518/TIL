a35eff31ac173b9933024cb21d6ff1ef4f297941
sangminLee <nalsm98@naver.com>
2019-03-26 12:24:05 +0900

    java 수업 자료 업로드

---
+++ Java/java_lecture/src/week_3/Quiz.java
@@ -0,0 +1,31 @@
+package week_3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Quiz {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static <T> void add(T country, T city, Map<T,T> map) {
+        map.put(country, city);
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<String, String>();
+
+        add("Korea", "Seoul", map);
+        add("일본", "도쿄", map);
+
+        System.out.print("입력: ");
+        String country = scanner.nextLine();
+
+        System.out.println("출력: "+ map.get(country));
+
+        System.out.print("입력: ");
+        country = scanner.nextLine();
+
+        System.out.println("출력: "+ map.get(country));
+    }
+}
