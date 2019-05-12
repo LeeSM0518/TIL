61a2f47cd9545ecb26ce2a819344c9d5eb29b8ab
sangminLee <nalsm98@naver.com>
2019-05-12 21:31:02 +0900

    Data Updata

--- Java/java_lecture/src/week6/Test.java
+++ Java/java_lecture/src/week6/Test.java
@@ -6,6 +6,7 @@ public class Test {
 
     private JButton button1;
     private JPanel mainPanel;
+    private JTextArea textArea1;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Quiz");
