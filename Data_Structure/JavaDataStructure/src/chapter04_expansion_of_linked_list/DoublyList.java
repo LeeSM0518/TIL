a4cc6db2f9880ee319db28f6fe86ceac5f3c7f6a
sangminLee <nalsm98@naver.com>
2019-05-17 15:27:20 +0900

    연결 리스트 확장 공부(40%)

--- Data_Structure/JavaDataStructure/src/chapter04_expansion_of_linked_list/DoublyList.java
+++ Data_Structure/JavaDataStructure/src/chapter04_expansion_of_linked_list/DoublyList.java
@@ -7,7 +7,13 @@ public class DoublyList<T> {
     DoublyList() {
         this.currentCount = 0;
         headerNode = new DoublyListNode<>();
-        headerNode.lLink = headerNode;
-        headerNode.rLink = headerNode;
+
+        // 처음 리스트를 만들때 headerNode 의 왼쪽과 오른쪽 노드를
+        // 자기 자신을 가리키도록 한다.
+        headerNode.lLink = this.headerNode;
+        headerNode.rLink = this.headerNode;
     }
+
+
+
 }
